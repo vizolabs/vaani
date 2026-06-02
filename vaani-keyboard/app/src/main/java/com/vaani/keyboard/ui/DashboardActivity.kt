a4caa124b38ced0a9168c67e33169f5bc22bfcd3
VixuxOG <contactzope@gmail.com>
2026-06-02 18:06:51 +0530

    refactor: extend BaseActivity in DashboardActivity

--- vaani-keyboard/app/src/main/java/com/vaani/keyboard/ui/DashboardActivity.kt
+++ vaani-keyboard/app/src/main/java/com/vaani/keyboard/ui/DashboardActivity.kt
@@ -2,21 +2,14 @@ package com.vaani.keyboard.ui
 
 import android.os.Bundle
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import com.vaani.keyboard.R
-import com.vaani.keyboard.util.Prefs
 import java.util.Calendar
 
-class DashboardActivity : AppCompatActivity() {
-
-    private lateinit var prefs: Prefs
+class DashboardActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
-
-        prefs = Prefs(this)
-        prefs.markActive()
 
         val hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
         val greeting = when {
