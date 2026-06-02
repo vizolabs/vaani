11361069cc39309145a004ea4e7282ace35d12ef
VixuxOG <contactzope@gmail.com>
2026-06-02 15:48:23 +0530

    chore: add onCreate lifecycle hook to VaaniApp

--- vaani-keyboard/app/src/main/java/com/vaani/keyboard/VaaniApp.kt
+++ vaani-keyboard/app/src/main/java/com/vaani/keyboard/VaaniApp.kt
@@ -2,4 +2,8 @@ package com.vaani.keyboard
 
 import android.app.Application
 
-class VaaniApp : Application()
+class VaaniApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
