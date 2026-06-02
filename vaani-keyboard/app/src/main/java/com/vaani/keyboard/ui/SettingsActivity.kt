9676fc95c3f8e332782f6c681ab2cf51a098cdef
VixuxOG <contactzope@gmail.com>
2026-06-02 18:06:56 +0530

    refactor: extend BaseActivity in SettingsActivity

--- vaani-keyboard/app/src/main/java/com/vaani/keyboard/ui/SettingsActivity.kt
+++ vaani-keyboard/app/src/main/java/com/vaani/keyboard/ui/SettingsActivity.kt
@@ -1,10 +1,9 @@
 package com.vaani.keyboard.ui
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.vaani.keyboard.R
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
